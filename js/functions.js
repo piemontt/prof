9d1fc8a1ec15a8bd30355577c636646b50f2cf24
Your Name <you@example.com>
2024-02-28 13:57:03 +0300

    com

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
 	$('.video__card').matchHeight();
+	$('.whatwedo__slider').slick({
+		slidesToShow: 3,
+		autoPlay: true,
+		
+	})
 	console.log('success')
 	jQuery(function(){
 		jQuery("#bgndVideo").YTPlayer({
