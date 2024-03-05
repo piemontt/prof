92fdcba4d1cd95c0428892b6c5225e53aad6220d
Your Name <you@example.com>
2024-03-05 14:21:41 +0300

    com

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,34 @@
 $( document ).ready(function() {
-	$('.video__card').matchHeight();
-/*	$('.tz-not__card__title').matchHeight();*/
 	$('.whatwedo__slider').slick({
 		slidesToShow: 3,
 		autoPlay: true,
 		prevArrow: '<img class="slick-prev" src="src/images/prew.svg">',
 		nextArrow: '<img class="slick-next" src="src/images/next.svg">',
-	})
-	console.log('success')
+	});
+	$('.consumers__slider').slick({
+		slidesToShow: 5,
+		arrows: false,
+		autoplay: true,
+	});
 	jQuery(function(){
 		jQuery("#bgndVideo").YTPlayer({
 			showControls: 0,
 		});
 	});
+	$('.big-manufacture__slider').slick({
+		slidesToShow: 3,
+		prevArrow: '<img class="slick-prev" src="src/images/prew.svg">',
+		nextArrow: '<img class="slick-next" src="src/images/next.svg">',
+	});
+		$('.blog__slider').slick({
+		slidesToShow: 3,
+		rows: 0,
+		prevArrow: '<img class="slick-prev" src="src/images/prew.svg">',
+		nextArrow: '<img class="slick-next" src="src/images/next.svg">',
+	});
+	$('.video__card').matchHeight();
+	$('.power___card').matchHeight({
+		byRow: false
+		});
 });
+ 
