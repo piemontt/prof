eec340f9fd200f42d9134563b36b9815b16ed939
Your Name <you@example.com>
2024-03-29 15:23:54 +0300

    adaptive&fixes

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,15 @@ $( document ).ready(function() {
 		autoPlay: true,
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_blue" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_blue" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
+		responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+			}
+		}
+			],
 	});
 	$('.consumers__slider').slick({
 		slidesToShow: 5,
@@ -11,9 +20,12 @@ $( document ).ready(function() {
 		autoplay: true,
 		responsive: [
 		{
-			breakpoint: 1200,
+			breakpoint: 576,
 			settings: {
-				slidesToShow: 3,
+				slidesToShow: 1,
+				arrows: false,
+				centerMode: true,
+				centerPadding: '50px',
 			}
 		}
 			],
@@ -27,13 +39,34 @@ $( document ).ready(function() {
 		slidesToShow: 3,
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
+		responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+			}
+		}
+			],
 	});
+
 		$('.blog__slider').slick({
 		slidesToShow: 3,
 		rows: 0,
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
+		responsive: [
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+			}
+		}
+			],
+
 	});
+
 		$('.process__slider').slick({
 		slidesToShow: 3,
 		rows: 0,
@@ -54,8 +87,12 @@ $( document ).ready(function() {
 	$('.tz-not__card').matchHeight();
 	$('.tz-not__card__title').matchHeight();
 
-	$(window).width() < 576 ? $('.header__menu').appendTo('.burger-menu') : false;
-	$('.burger-button').click(function() {
-		$('.burger-menu,.burger-button').toggleClass('active')
+	$('.burger-button').on("click", function(){
+		$('.header__menu,.burger-button').toggleClass('active');
 	});
+
+	if ($(window).width() < 576) {
+		$('.header__buttons-wrapper').appendTo($('.header__menu'));
+		$('.header__phone-email-wrap').appendTo($('.header__menu'));
+	};
 });
