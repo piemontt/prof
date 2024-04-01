cbe1973e4354ff846f9930d4697e8e782d0ead02
Your Name <you@example.com>
2024-04-01 18:07:05 +0300

    final v1

--- js/functions.js
+++ js/functions.js
@@ -89,16 +89,50 @@ $( document ).ready(function() {
 
 		$('.process__slider').slick({
 		slidesToShow: 3,
-
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+			}
+		}
+			],
+
 	});
 		$('.types__slider').slick({
 		slidesToShow: 3,
-
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
-	});
+		responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						arrows: false,
+					}
+				},
+				{
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 1,
+						arrows: false,
+					}
+				}
+					],
+
+		});
 	$('.video__card').matchHeight();
 	$('.power___card').matchHeight({
 		byRow: false
@@ -113,6 +147,7 @@ $( document ).ready(function() {
 	$('.burger-button').on("click", function(){
 		$('.header__menu,.burger-button').toggleClass('active');
 		$('header').toggleClass('header-padding');
+		$('html,body').toggleClass('body-hidden');
 	});
 
 	if ($(window).width() < 576) {
