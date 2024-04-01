d52cea627002e33186626a5c7375aa1a1f593019
Your Name <you@example.com>
2024-04-01 15:58:17 +0300

    fix

--- js/functions.js
+++ js/functions.js
@@ -6,12 +6,20 @@ $( document ).ready(function() {
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_blue" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
 		responsive: [
 		{
+			breakpoint: 768,
+			settings: {
+			arrows: false,
+			slidesToScroll: 3,
+			}
+		},
+		{
 			breakpoint: 576,
 			settings: {
 				slidesToShow: 1,
 				arrows: false,
 			}
 		}
+
 			],
 	});
 	$('.consumers__slider').slick({
@@ -35,6 +43,7 @@ $( document ).ready(function() {
 	});
 	$('.big-manufacture__slider').slick({
 		slidesToShow: 3,
+		autoPlay: true,
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
 		responsive: [
@@ -50,12 +59,25 @@ $( document ).ready(function() {
 
 		$('.blog__slider').slick({
 		slidesToShow: 3,
-		rows: 0,
+		autoPlay: true,
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
 		responsive: [
 		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
 			breakpoint: 576,
+			settings: {
+				slidesToShow: 2,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 420,
 			settings: {
 				slidesToShow: 1,
 				arrows: false,
@@ -67,18 +89,21 @@ $( document ).ready(function() {
 
 		$('.process__slider').slick({
 		slidesToShow: 3,
-		rows: 0,
+
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
 	});
 		$('.types__slider').slick({
 		slidesToShow: 3,
-		rows: 0,
+
 		prevArrow: '<svg class="slick-prev" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" d="M9.07091 1.3207L1.99983 8.39177M1.99983 8.39177L28.7839 8.39236M1.99983 8.39177L9.0709 15.4628" stroke="#0F1330" opacity="0.5" stroke-width="1.5"></svg>',
 		nextArrow: '<svg class="slick-next" width="29" height="16" viewBox="0 0 29 16" xmlns="http://www.w3.org/2000/svg"><path class="arrow-hover_black" opacity="0.5" d="M19.713 1.3207L26.7841 8.39177M26.7841 8.39177L-1.3154e-07 8.39236M26.7841 8.39177L19.713 15.4628" stroke="#0F1330" stroke-width="1.5"/></svg>',
 	});
 	$('.video__card').matchHeight();
 	$('.power___card').matchHeight({
+		byRow: false
+		});
+	$('.whatwedo__slider__slide__card').matchHeight({
 		byRow: false
 		});
 	
